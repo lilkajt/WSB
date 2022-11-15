6fc3ccb2652a01a36b37e92d3f885246578066f1
lilkajt <kajetan.szlenzak@gmail.com>
2022-11-15 15:02:27 +0100

    Exercise #2

---
+++ 2st exercise/ConsoleApp2/Program.cs
@@ -0,0 +1,102 @@
+﻿Console.WriteLine("Please input number: ");
+/*
+string b = Console.ReadLine();
+if (b == null)
+{
+    Console.WriteLine("Input is empty!Try again");
+    Console.WriteLine();
+} else if (int.TryParse(b))
+{
+    int c = int.Parse(b);
+}
+else
+{
+    Console.WriteLine("cant convert");
+    Console.ReadLine();
+}
+*/
+string Raw_a = Console.ReadLine();
+/*
+if (Raw_a == "")
+{
+    Console.WriteLine("User input is empty");
+    Console.ReadLine();
+}
+else
+{
+    bool TryParseuserInputA = int.TryParse(Raw_a, out int a);
+    if (TryParseuserInputA)
+    {
+        if (a % 2 == 0)
+        {
+            Console.WriteLine($"{a} is even");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine($"{a} is odd");
+            Console.ReadLine();
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Input is not a number");
+        Console.ReadLine();
+    }
+}
+*/
+
+
+string Raw_b = Console.ReadLine();
+string Raw_c = Console.ReadLine();
+
+/*
+if (Raw_a == "" && Raw_b == "")
+{
+    Console.WriteLine("User input empty!");
+    Console.ReadLine();
+}
+else
+{
+    bool TryParseuserInputA= int.TryParse(Raw_a, out int x);
+    bool TryParseuserInputB= int.TryParse(Raw_b, out int z);
+    if (TryParseuserInputA && TryParseuserInputB)
+    {
+        double result = (x * z) / (x + z);
+        Console.WriteLine($"Result of equation is {result}");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("User did not input number");
+        Console.ReadLine();
+    }
+}
+*/
+/*
+if (Raw_a == "" && Raw_b == "" && Raw_c == "")
+{
+    Console.WriteLine("User input empty!");
+    Console.ReadLine();
+}
+else
+{
+    bool TryParseuserInputA= int.TryParse(Raw_a, out int x);
+    bool TryParseuserInputB= int.TryParse(Raw_b, out int z);
+    bool TryParseuserInputC = int.TryParse(Raw_b, out int y);
+
+    if (TryParseuserInputA && TryParseuserInputB && TryParseuserInputC)
+    {
+        int[] arrey = {x, y, z};
+        Console.WriteLine($"Result of equation is {result}");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("User did not input number");
+        Console.ReadLine();
+    }
+}
+
+*/
