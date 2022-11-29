98a8112d2f87340d18037b4db9b6f3d142801e28
lilkajt <kajetan.szlenzak@gmail.com>
2022-11-29 13:57:37 +0100

    Initial commit + Pig latin

---
+++ lesson exercise 1/exercise1lesson/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("User input word: ");
+string ask = Console.ReadLine();
+string WordWithOutFirstLatter = ask.Substring(1);
+//Console.WriteLine(ask.Substring(1));
+Console.WriteLine(WordWithOutFirstLatter+ask[0]+"ay");
