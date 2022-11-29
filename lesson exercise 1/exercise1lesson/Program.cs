8d2ae00f82b45458b9012ae3d8bcb9f006dab0ce
lilkajt <kajetan.szlenzak@gmail.com>
2022-11-29 14:29:47 +0100

    Sentence piglatin

--- lesson exercise 1/exercise1lesson/Program.cs
+++ lesson exercise 1/exercise1lesson/Program.cs
@@ -1,5 +1,15 @@
 ﻿Console.Write("User input word: ");
 string ask = Console.ReadLine();
+//EXercise 1
+/*
 string WordWithOutFirstLatter = ask.Substring(1);
 //Console.WriteLine(ask.Substring(1));
 Console.WriteLine(WordWithOutFirstLatter+ask[0]+"ay");
+*/
+//hi whats up
+//ihay hatsway puay
+string[] words = ask.Split();
+for (int i = 0; i < words.Length; i++)
+{
+    Console.Write(words[i].Substring(1) + words[i][0] +"ay ");
+}
